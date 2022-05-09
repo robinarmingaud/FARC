2ed944e50aa5041eff229cccb5d8cd3346f7c271
robinarmingaud <robin.armingaud@imt-atlantique.net>
2022-05-09 17:02:52 +0200

    Correctif traduction

--- farc-master/farc/apps/calculator/report_generator.py
+++ farc-master/farc/apps/calculator/report_generator.py
@@ -21,7 +21,7 @@ from farc.apps.calculator import markdown_tools
 
 from ... import monte_carlo as mc
 from .model_generator import FormData, _DEFAULT_MC_SAMPLE_SIZE
-from .DEFAULT_DATA import ACTIVITY_TYPES, set_locale
+from .DEFAULT_DATA import ACTIVITY_TYPES, MONTH_NAMES, set_locale
 from ... import dataclass_utils
 
 
@@ -316,11 +316,13 @@ class ReportGenerator:
     locale = tornado.locale.get()
     _ = locale.translate
     ACTIVITY_TYPES = ACTIVITY_TYPES
+    MONTH_NAMES = MONTH_NAMES
 
     def set_locale(self, locale):
         self.locale = locale
         self._ = locale.translate
         self.ACTIVITY_TYPES = set_locale(locale)['ACTIVITY_TYPES']
+        self.MONTH_NAMES = set_locale(locale)['MONTH_NAMES']
 
     def build_report(
             self,
@@ -378,6 +380,7 @@ class ReportGenerator:
         env.filters['JSONify'] = json.dumps
         env.globals['_'] = self._
         env.globals['ACTIVITY_TYPES'] = self.ACTIVITY_TYPES
+        env.globals['MONTH_NAMES'] = self.MONTH_NAMES
         return env
 
     def render(self, context: dict) -> str:
