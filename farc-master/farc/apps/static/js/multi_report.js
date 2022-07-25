4572fdfe4a61cd15dfd791311c389d480b2d18d8
robin.armingaud <robin.armingaud@ingenica.fr>
2022-07-25 12:16:14 +0200

    csv name with date and table title

--- farc-master/farc/apps/static/js/multi_report.js
+++ farc-master/farc/apps/static/js/multi_report.js
@@ -1,6 +1,8 @@
 $(document).ready(function () { $(".btn-link").on('click', function() {
+    var utc = new Date().toJSON()
     var parent = $(this).parent().parent()
     var table = parent.find("table")
-    table.table2csv();
+    var title = parent.find("#DivProbabilityOfInfection").attr('value');
+    table.table2csv({filename: (utc+title+'.csv').replace(/:/g,"_")});
     })
 })
